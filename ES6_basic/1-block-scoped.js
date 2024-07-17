cd5ec38c61e244b9172d68248b6f0fc740df0639
HolbieWan <8841@holbertonstudents.com>
2024-07-17 12:48:22 +0200

    Add: 1-block-scoped.js, 1-main.js

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,12 +1,13 @@
-export default function taskBlock(trueOrFalse) {
+export default function taskBlock(trueOrFalse) 
+{
     const task = false;
     const task2 = true;
   
-    if (trueOrFalse) {
+    if (trueOrFalse)
+    {
       const condition_task = true;
       const condition_task2 = false;
     }
   
     return [task, task2];
-  }
-  
+}
