97564136d17cd7df787803f1f8c56a180f2e122c
HolbieWan <8841@holbertonstudents.com>
2024-07-18 14:22:24 +0200

    Add: 10-getFullBudget.js, 10-main.js

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -2,7 +2,5 @@ export default function appendToEachArrayValue(array, appendString) {
     for (let [index, value] of array.entries()) {
       array[index] = appendString + value;
     }
-  
     return array;
 }
-  
