678b989688c4e4ac43cb1740a67aecec335ad157
HolbieWan <8841@holbertonstudents.com>
2024-07-18 10:48:43 +0200

    Update task 0-6

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,13 +1,12 @@
 export default function taskBlock(trueOrFalse) 
 {
-    const task = false;
-    const task2 = true;
-  
-    if (trueOrFalse)
-    {
-      const condition_task = true;
-      const condition_task2 = false;
-    }
-  
-    return [task, task2];
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const condition_task = true;
+    const condition_task2 = false;
+  }
+
+  return [task, task2];
 }
