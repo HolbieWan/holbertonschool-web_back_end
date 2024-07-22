155e3178fbbe7d7e722cdbb838417982d5575ceb
HolbieWan <8841@holbertonstudents.com>
2024-07-22 15:14:38 +0200

    Add: 5-typed_arrays.js, 5-main.js

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,12 @@
+export default function createInt8TypedArray(length, position, value) {
+  let buffer = new ArrayBuffer(length);
+  let dataView = new DataView(buffer);
+
+  if (position >= length || position < 0) {
+    throw new Error("Position outside range");
+  }
+
+  dataView.setInt8(position, value);
+
+  return dataView;
+}
