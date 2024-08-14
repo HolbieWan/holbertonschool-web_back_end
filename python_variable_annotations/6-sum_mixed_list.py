8ead2547d766bf8eca50821b5cef17a7abcc5a72
HolbieWan <8841@holbertonstudents.com>
2024-08-14 15:13:05 +0200

    Add: 6-sum_mixed_list.py, 6-main.py

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Annotate the below function’s parameters and
+ return values with the appropriate types"""
+
+
+def sum_mixed_list(mxd_lst: list[int | float]) -> float:
+    """Return the sum of a list of floats and integers"""
+    return sum(mxd_lst)
