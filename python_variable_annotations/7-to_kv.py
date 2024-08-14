acc88869edbe4fbcbfd0cea9951fb3ed45d636db
HolbieWan <8841@holbertonstudents.com>
2024-08-14 15:58:48 +0200

    Add: 6-sum_mixed_list.py, 7-to_kv.py, 7-main.py

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""type-annotated function to_kv that takes a string k and an int OR float
-v as argumentsand returns a tuple. The first element of the tuple is the
-string k.The second element is the square of the int/float v and should be
+"""Type-annotated function to_kv that takes a string k and an int OR float
+v as arguments and returns a tuple. The first element of the tuple is the
+string k. The second element is the square of the int/float v and should be
 annotated as a float.
 """
 
@@ -9,9 +9,9 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """type-annotated function to_kv that takes a string k and an int OR float
-    v as argumentsand returns a tuple. The first element of the tuple is the
-    string k.The second element is the square of the int/float v and should be
+    """Type-annotated function to_kv that takes a string k and an int OR float
+    v as arguments and returns a tuple. The first element of the tuple is the
+    string k. The second element is the square of the int/float v and should be
     annotated as a float.
     """
-    return Tuple[k, v * v]
+    return (k, float(v * v))
