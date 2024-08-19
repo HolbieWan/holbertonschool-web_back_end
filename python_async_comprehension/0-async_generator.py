bf7ed1a4311443c82ec304905d2f82662dc83b52
HolbieWan <8841@holbertonstudents.com>
2024-08-19 10:29:25 +0200

    Add: README.md,  0-main.py,   0-async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,8 +4,10 @@
 import asyncio
 import random
 
+
 async def async_generator():
+    """Async generator function that yelds a random number between 0 an 10"""
     for i in range(10):
         i = random.uniform(0, 10)
         await asyncio.sleep(1)
-        yield(i)
+        yield (i)
