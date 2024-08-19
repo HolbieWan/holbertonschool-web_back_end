02d7291ab2c44e1cc4d33eaab0cf94dae49d4e87
HolbieWan <8841@holbertonstudents.com>
2024-08-19 11:38:01 +0200

    Update: 1-async_comprehension.py

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -3,9 +3,10 @@
 
 import asyncio
 import random
+
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> float:
+async def async_comprehension() -> list[float]:
     """Async function that returns yielded numbers from async_generator()"""
     return [i async for i in async_generator()]
