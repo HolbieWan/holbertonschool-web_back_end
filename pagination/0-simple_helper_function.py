efa9531a5abbca1c3054ef28c0797f388fc63280
HolbieWan <8841@holbertonstudents.com>
2024-08-20 14:36:25 +0200

    Add:  0-main.py,  0-simple_helper_function.py, Popular_Baby_names.csv,  README.md

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Pagination"""
 
+
 def index_range(page: int, page_size: int):
     """Helper function that return a tuple of size two containing a start index
     and an end index corresponding to the range of indexes to return in a list
     for those particular pagination parameters."""
     a = (page - 1) * page_size
     b = page * page_size
-    return(a, b)
+    return (a, b)
