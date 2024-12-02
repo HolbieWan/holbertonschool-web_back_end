d6f3e55cea13ee2a4fb33bdb2be51215dbfe1535
HolbieWan <8841@holbertonstudents.com>
2024-12-02 17:34:43 +0100

    Add: 2-floor.py, 2-main.py, 3-main.py, 3-to_str.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module with a function that returns the floor of a float"""
+
+
+def floor(n: float) -> int:
+    """function that takes a float n as argument
+    and returns the floor of the float"""
+    return int(n)
