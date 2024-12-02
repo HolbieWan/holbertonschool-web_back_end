4a3d9921120473b783083775764f1fa5ff3470ce
HolbieWan <8841@holbertonstudents.com>
2024-12-02 17:12:12 +0100

    Add: .gitignore,  0-add.py,  0-main.py

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module with function to add 2 int"""
+
+
+def add(a: int, b: int) -> int:
+    """function add that takes a float a and a float b as arguments
+    and returns their sum as a float."""
+    return a + b
