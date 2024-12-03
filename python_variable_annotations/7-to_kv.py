f960c687fbda91cf880fe7132ade45d8e8c85da3
HolbieWan <8841@holbertonstudents.com>
2024-12-03 11:17:08 +0100

    Update: 7-to_kv.py: use Union for type of argument v instead of |

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,10 +2,10 @@
 """Module with a function that takes 2 arguments
 and returns them in a tuple"""
 
-from typing import Tuple
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: int | float) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """type-annotated function to_kv that takes a string k
     and an int OR float v as arguments and returns a tuple"""
     return (k, float(v**2))
