8a88226b8d961f7de5a0a6cee0ce8ca013d0d9fb
HolbieWan <8841@holbertonstudents.com>
2024-12-04 16:10:12 +0100

    Add: 4-main.py, 4-tasks.py, Update: 1-concurrent_coroutines.py: correct pycodestyle

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -4,17 +4,17 @@
 import asyncio
 from typing import List
 bubble_sort = __import__('bubble_sort').bubble_sort
-wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Function that takes in 2 int arguments (n and max_delay)
     spawns wait_random n times with max_delay
     and returns the list of all the delays (float values)"""
     i = 0
     delays_list = []
     while (i < n):
-        result = wait_random(max_delay)
+        result = task_wait_random(max_delay)
         delays_list.append(result)
         i += 1
     result_list = await asyncio.gather(*delays_list)
