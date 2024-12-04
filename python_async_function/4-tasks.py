39fee6fda2d177776852b59846767e95b5344f51
HolbieWan <8841@holbertonstudents.com>
2024-12-04 16:21:37 +0100

    Update: 4-task.py

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -22,5 +22,5 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     return result_list
 
 # better way:
-    # coroutines = [wait_random(max_delay) for _ in range(n)]
-    # delays = await asyncio.gather(*coroutines)
+    # task = [task_wait_random(max_delay) for _ in range(n)]
+    # delays = await asyncio.gather(*task)
