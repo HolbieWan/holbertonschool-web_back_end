64c7a277e5a8fe5706c93cb89310f3bebbab2a2d
HolbieWan <8841@holbertonstudents.com>
2024-12-09 17:11:08 +0100

    Add: 2-hypermedia_pagination.py

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -2,7 +2,7 @@
 '''Module with an helper function: index_range'''
 import csv
 import math
-from typing import Tuple, List, Dict
+from typing import Tuple, List
 
 
 def index_range(page: int, page_size: int) -> tuple:
@@ -34,6 +34,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Method that takes two integer arguments: page with default value 1
+        and page_size with default value 10.
+        Returns a page of the dataset"""
         assert (isinstance(page, int) and page > 0)
         assert (isinstance(page_size, int) and page_size > 0)
         data_list = self.dataset()
@@ -42,6 +45,19 @@ class Server:
             return data_list[start_index:end_index]
         else:
             return []
-        
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict{int, int, List[List], int, int, int}:
-        
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Method that return a dictionary with page metadatas"""
+        page_data = self.get_page(page, page_size)
+        total_pages = len(self.dataset()) / page_size
+        next_page = page + 1 if page + 1 < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+            "page_size": page_size,
+            "page": page,
+            "data": page_data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
+        }
