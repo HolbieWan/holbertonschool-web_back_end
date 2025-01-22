e657713fc8f7b6fa9fedff66ceda6d1cc3887aff
HolbieWan <8841@holbertonstudents.com>
2025-01-22 21:04:52 +0100

    Update: 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,16 +1,16 @@
 // Program to read user input from stdin and display a message
 
 // Display welcome message
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
 // Event listener for input
-process.stdin.on("data", (data) => {
-  const name = data.toString();
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
   process.exit();
 });
 
 // Event listener for program exit
-process.on("exit", () => {
-  console.log("This important software is now closing");
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
